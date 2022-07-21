f5f9b02cf805a0843b247941850d55c6a7c96dc9
AustinGTI <gathiiaustin@gmail.com>
2022-07-21 15:24:01 +0300

    basic layout design

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="main"></div>;
+  useEffect(() => {
+    let fBox = document.querySelector("div.formatBox");
+    let fSize = fBox.getBoundingClientRect();
+    console.log(fSize.width, fSize.height);
+  }, []);
+
+  return (
+    <div className="appBg">
+      <div className="logoBox"></div>
+      <div className="appBox">
+        <div className="paletteBox">
+          <div className="addRightBtn addBtn btn"></div>
+          <div className="addLeftBtn addBtn btn"></div>
+          <div className="downloadBtn btn"></div>
+          {Array.from(Array(3).keys()).map((v, vi) => (
+            <div key={vi} className="colorBox">
+              <div className="colorDetailsBox"></div>
+            </div>
+          ))}
+        </div>
+        <div className="tunersBox">
+          <div className="hexBox"></div>
+          <div className="slidersBox">
+            {Array.from(Array(4).keys()).map((v, vi) => (
+              <div key={vi} className="formatBox"></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
