d5a21672508231fa37125cf9c69b35e87996c24e
AustinGTI <gathiiaustin@gmail.com>
2022-11-09 17:29:04 +0300

    pane/tree active & hover design
    came up with design for the pane and tree while active and hovered.. Have not yet implemented but should be completed in an hour or 2 tommorrow

--- src/App/Easel/ColorPane/ColorDetails.js
+++ src/App/Easel/ColorPane/ColorDetails.js
@@ -1,95 +1,83 @@
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import "./ColorDetails.scss";
 import useAppContext, {
-  getPaneColor,
+    getPaneColor,
 } from "../../../AuxFunctions/useAppContext";
 import formatColor from "../../../AuxFunctions/formatColor";
-import { ReactComponent as UndoBtn } from "../../../Data/Icons/Buttons/undoBtn.svg";
-import { ReactComponent as RedoBtn } from "../../../Data/Icons/Buttons/redoBtn.svg";
-import { ReactComponent as DeleteBtn } from "../../../Data/Icons/Buttons/deleteBtn.svg";
-import { ReactComponent as MoveBtn } from "../../../Data/Icons/Buttons/moveBtn.svg";
-import { getContrastColor } from "../../../AuxFunctions/filterColor";
+import {ReactComponent as UndoBtn} from "../../../Data/Icons/Buttons/undoBtn.svg";
+import {ReactComponent as RedoBtn} from "../../../Data/Icons/Buttons/redoBtn.svg";
+import {ReactComponent as DeleteBtn} from "../../../Data/Icons/Buttons/deleteBtn.svg";
+import {ReactComponent as MoveBtn} from "../../../Data/Icons/Buttons/moveBtn.svg";
+import {getContrastColor} from "../../../AuxFunctions/filterColor";
 
 function cleanFormats(format, value) {
-  if (["rgb", "hsv", "hsl", "cmyk"].includes(format)) {
-    return `${format}(${value.join(",")})`;
-  }
-  return value;
+    if (["rgb", "hsv", "hsl", "cmyk"].includes(format)) {
+        return `${format}(${value.join(",")})`;
+    }
+    return value;
 }
 
-export default function ColorDetails({ pane, isActive }) {
-  let formats = getPaneColor(pane);
-  let detailVisible = useAppContext()[0].colorDetailVisible;
-  let formatKeys = Object.keys(formats).filter((v) => v !== "cls");
-  let emphasis = isActive ? 40 : 20;
-  let fontColor = getContrastColor("hsl", formats.hsl, emphasis);
-  // formats.hsl[2] > 50
-  //   ? [
-  //       formats.hsl[0],
-  //       formats.hsl[1].toString() + "%",
-  //       (formats.hsl[2] - emphasis).toString() + "%",
-  //     ]
-  //   : [
-  //       formats.hsl[0],
-  //       formats.hsl[1].toString() + "%",
-  //       (formats.hsl[2] + emphasis).toString() + "%",
-  //     ];
-  const colDetailsBox = useRef(null);
-  useEffect(() => {
-    const appBox = document.querySelector(".appBg > .appBox");
 
-    if (isActive) {
-      appBox.style.setProperty("--primary-active-color", formats.hex);
-      appBox.style.setProperty("--secondary-active-color", fontColor);
-    }
+function ColorDetail({format, value}) {
+    const doNotLabel = ["col", "cls"];
+    return (
+        <div className={"colorDetailBox"}>
+            {(!doNotLabel.includes(format)) ? <div className={"detailFormatBox"}>{format}</div> : <></>}
+            <div className={"detailValueBox"}>{(Array.isArray(value)) ? value.join(',') : value}</div>
+        </div>
+    )
+}
 
-    colDetailsBox.current.parentElement.style.setProperty(
-      "--primary-color",
-      formats.hex
-    );
-    colDetailsBox.current.parentElement.style.setProperty(
-      "--secondary-color",
-      fontColor
-    );
-  }, [isActive, formats, colDetailsBox]);
+export default function ColorDetails({pane, isActive}) {
+    let formats = getPaneColor(pane);
+    let detailVisible = useAppContext()[0].colorDetailVisible;
+    let formatKeys = Object.keys(formats).filter((v) => v !== "cls");
+    let emphasis = isActive ? 40 : 20;
+    let fontColor = getContrastColor("hsl", formats.hsl, emphasis);
+    // formats.hsl[2] > 50
+    //   ? [
+    //       formats.hsl[0],
+    //       formats.hsl[1].toString() + "%",
+    //       (formats.hsl[2] - emphasis).toString() + "%",
+    //     ]
+    //   : [
+    //       formats.hsl[0],
+    //       formats.hsl[1].toString() + "%",
+    //       (formats.hsl[2] + emphasis).toString() + "%",
+    //     ];
+    const colDetailsBox = useRef(null);
+    useEffect(() => {
+        const appBox = document.querySelector(".appBg > .appBox");
 
-  return (
-    <>
-{/*
-      <div className={`bracer left ${isActive ? "active" : ""}`}>
-         to be replaced with svgs when working on animations
-        <div className="indicator"></div>
-      </div>
-*/}
-      <div className={`colorDetailsBox ${detailVisible ? "visible" : "hidden"}`} ref={colDetailsBox}>
-        {/*<div className="btn panelBtn moveBtn">
+        if (isActive) {
+            appBox.style.setProperty("--primary-active-color", formats.hex);
+            appBox.style.setProperty("--secondary-active-color", fontColor);
+        }
+
+        colDetailsBox.current.parentElement.style.setProperty(
+            "--primary-color",
+            formats.hex
+        );
+        colDetailsBox.current.parentElement.style.setProperty(
+            "--secondary-color",
+            fontColor
+        );
+    }, [isActive, formats, colDetailsBox]);
+
+    return (
+        <>
+            <div className={`colorDetailsBox ${detailVisible ? "visible" : "hidden"}`} ref={colDetailsBox}>
+                {/*<div className="btn panelBtn moveBtn">
           <MoveBtn />
         </div>*/}
-        {formatKeys.map((v, vi) => (
-          <div className="format" key={vi}>
-            <span>{cleanFormats(v, formats[v])}</span>
-          </div>
-        ))}
-{/*
-        <div className="keyBtnBox">
-          <div className="btn keyBtn undoBtn">
-            <UndoBtn />
-          </div>
-          <div className="btn keyBtn deleteBtn">
-            <DeleteBtn />
-          </div>
-          <div className="btn keyBtn redoBtn">
-            <RedoBtn />
-          </div>
-        </div>
-*/}
+                {formatKeys.map((v, vi) => (
+                    // <div className="format" key={vi}>
+                    //   <span>{cleanFormats(v, formats[v])}</span>
+                    // </div>
+                    <ColorDetail value={formats[v]} format={v}/>
+                ))}
 
-      </div>
-{/*
-      <div className={`bracer right ${isActive ? "active" : ""}`}>
-        <div className="indicator"></div>
-      </div>
-*/}
-    </>
-  );
+            </div>
+        </>
+    );
 }
--- src/App/Easel/Easel.js
+++ src/App/Easel/Easel.js
@@ -2,107 +2,105 @@ import React, {useEffect, useReducer, useRef} from "react";
 import useAppContext, {getPaneColor} from "../../AuxFunctions/useAppContext";
 import ColorPane from "./ColorPane/ColorPane";
 
-import { ReactComponent as DownloadBtn } from "../../Data/Icons/Buttons/downloadBtn.svg";
+import {ReactComponent as DownloadBtn} from "../../Data/Icons/Buttons/downloadBtn.svg";
 import {ReactComponent as DeleteBtn} from "../../Data/Icons/Buttons/deleteBtn.svg";
 import {ReactComponent as HideBtn} from "../../Data/Icons/Buttons/hideBtn.svg";
 import {ReactComponent as ShowBtn} from "../../Data/Icons/Buttons/showBtn.svg";
-import { ReactComponent as Logo } from "../../Data/Icons/Logos/logoV2.svg";
+import {ReactComponent as Logo} from "../../Data/Icons/Logos/logoV2.svg";
 
 import "./Easel.scss";
 import {hexToHSL, hexToRGB} from "../../AuxFunctions/formatColor";
 
 export default function Palette() {
-  const [appData,setter,pane] = useAppContext();
-  const easelBtnBox = useRef(null);
-  const logoBox = useRef(null);
+    const [appData, setter, pane] = useAppContext();
+    const easelBtnBox = useRef(null);
+    const logoBox = useRef(null);
+    useEffect(() => {
+        const deletePane = (e) => {
+            setter({command: "deletePane", id: pane.paneId});
+        };
+        const hideUnhidePane = (e) => {
+            setter({command: "toggleVisible", id: pane.paneId});
+        };
+        const [hideBtn, deleteBtn, downloadBtn] = easelBtnBox.current.querySelectorAll("div.btn");
+        deleteBtn.addEventListener("click", deletePane);
+        hideBtn.addEventListener("click", hideUnhidePane);
+        return () => {
+            deleteBtn.removeEventListener("click", deletePane);
+            hideBtn.removeEventListener("click", hideUnhidePane);
+        }
+        //pane deletion
+    });
 
-  useEffect(()=>{
-      const deletePane = (e) => {
-          setter({ command: "deletePane", id: pane.paneId });
-      };
-      const hideUnhidePane = (e) => {
-          setter({command:"toggleVisible",id: pane.paneId});
-      }
-      const [hideBtn,deleteBtn,downloadBtn] = easelBtnBox.current.querySelectorAll("div.btn");
-      deleteBtn.addEventListener("click",deletePane);
-      hideBtn.addEventListener("click",hideUnhidePane);
-      return ()=>{
-          deleteBtn.removeEventListener("click",deletePane);
-          hideBtn.removeEventListener("click",hideUnhidePane);
-      }
-      //pane deletion
-  })
-
-    useEffect(()=>{
+    useEffect(() => {
         //dynamic logo
-        const logoCircles = Array.from(logoBox.current.querySelectorAll("svg circle")).sort((a,b)=>b.r.baseVal.value-a.r.baseVal.value);
-        logoCircles.forEach((v,vi)=>{
-            if (appData.colorPanes.length > vi){
-                v.style.fill=getPaneColor(appData.colorPanes[vi]).hex;
+        const logoCircles = Array.from(logoBox.current.querySelectorAll("svg circle")).sort((a, b) => b.r.baseVal.value - a.r.baseVal.value);
+        logoCircles.forEach((v, vi) => {
+            if (appData.colorPanes.length > vi) {
+                v.style.fill = getPaneColor(appData.colorPanes[vi]).hex;
+            } else {
+                v.style.fill = "transparent";
             }
-            else{
-                v.style.fill="transparent";
-            }
-        })
-        let activeColor = getPaneColor(appData.colorPanes.find((v)=>v.paneId === appData.activePaneIdx));
-        let nBgColor = activeColor.rgb.map(v=>parseInt(255*9/10 + v/10));
-        let bgDarkHex = hexToRGB(nBgColor.map(v=>parseInt(v*0.94)),true);
-        let bgLightHex = hexToRGB(nBgColor.map(v=>Math.max(0,parseInt(v*1.06))),true);
-        let nBgColorHex=hexToRGB(nBgColor,true);
+        });
+        let activeColor = getPaneColor(appData.colorPanes.find((v) => v.paneId === appData.activePaneIdx));
+        let nBgColor = activeColor.rgb.map(v => parseInt(255 * 9 / 10 + v / 10));
+        let bgDarkHex = hexToRGB(nBgColor.map(v => parseInt(v * 0.94)), true);
+        let bgLightHex = hexToRGB(nBgColor.map(v => Math.max(0, parseInt(v * 1.06))), true);
+        let nBgColorHex = hexToRGB(nBgColor, true);
         let btnBgColor = `rgba(${activeColor.rgb.join(',')},0.1)`;
-        let btnFillColor = hexToHSL(activeColor.hsl.map((v,vi)=>( vi==2 )?50:v),true);
+        let btnFillColor = hexToHSL(activeColor.hsl.map((v, vi) => (vi === 2) ? 50 : v), true);
 
-        console.log(nBgColor,bgDarkHex,bgLightHex,btnBgColor,btnFillColor);
-        document.documentElement.style.setProperty('--main-bg-color',nBgColorHex);
-        document.documentElement.style.setProperty('--dark-bg-color',bgDarkHex);
-        document.documentElement.style.setProperty('--light-bg-color',bgLightHex);
-        document.documentElement.style.setProperty('--btn-bg-color',btnBgColor);
-        document.documentElement.style.setProperty('--btn-fill-color',btnFillColor);
+        console.log(nBgColor, bgDarkHex, bgLightHex, btnBgColor, btnFillColor);
+        document.documentElement.style.setProperty('--main-bg-color', nBgColorHex);
+        document.documentElement.style.setProperty('--dark-bg-color', bgDarkHex);
+        document.documentElement.style.setProperty('--light-bg-color', bgLightHex);
+        document.documentElement.style.setProperty('--btn-bg-color', btnBgColor);
+        document.documentElement.style.setProperty('--btn-fill-color', btnFillColor);
 
-    },[appData]);
-  /*useEffect(() => {
-    const addLeftBtn = document.querySelector(".addLeftBtn");
-    const addRightBtn = document.querySelector(".addRightBtn");
-    const panes = document.querySelectorAll("div.colorBox");
+    }, [appData]);
+    /*useEffect(() => {
+      const addLeftBtn = document.querySelector(".addLeftBtn");
+      const addRightBtn = document.querySelector(".addRightBtn");
+      const panes = document.querySelectorAll("div.colorBox");
 
-    //add Pane function
-    const addPaneLeft = () => {
-      console.log("add left");
-      setAppData({ command: "addPane", direction: 0 });
-    };
-    const addPaneRight = () => {
-      setAppData({ command: "addPane", direction: 1 });
-    };
+      //add Pane function
+      const addPaneLeft = () => {
+        console.log("add left");
+        setAppData({ command: "addPane", direction: 0 });
+      };
+      const addPaneRight = () => {
+        setAppData({ command: "addPane", direction: 1 });
+      };
 
 
-    //adding event listeners
-    addLeftBtn.addEventListener("click", addPaneLeft);
-    addRightBtn.addEventListener("click", addPaneRight);
-    return () => {
-      addLeftBtn.removeEventListener("click", addPaneLeft);
-      addRightBtn.removeEventListener("click", addPaneRight);
-    };
-  }, [appData, setAppData]);*/
-  return (
-    <div className="easelBox">
-      <div className="logoBox" ref={logoBox}>
-        <Logo />
+      //adding event listeners
+      addLeftBtn.addEventListener("click", addPaneLeft);
+      addRightBtn.addEventListener("click", addPaneRight);
+      return () => {
+        addLeftBtn.removeEventListener("click", addPaneLeft);
+        addRightBtn.removeEventListener("click", addPaneRight);
+      };
+    }, [appData, setAppData]);*/
+    return (
+        <div className="easelBox">
+            <div className="logoBox" ref={logoBox}>
+                <Logo/>
 
-      </div>
-        <div className={"easelBtnBox"} ref={easelBtnBox}>
-           <div className={"hideBtn btn"}>
-               {appData.colorDetailVisible ? <HideBtn/> : <ShowBtn/>}
-           </div>
-            <div className={"deleteBtn btn"}>
-                <DeleteBtn/>
             </div>
-            <div className="downloadBtn btn">
-                <DownloadBtn />
+            <div className={"easelBtnBox"} ref={easelBtnBox}>
+                <div className={"hideBtn btn"}>
+                    {appData.colorDetailVisible ? <HideBtn/> : <ShowBtn/>}
+                </div>
+                <div className={"deleteBtn btn"}>
+                    <DeleteBtn/>
+                </div>
+                <div className="downloadBtn btn">
+                    <DownloadBtn/>
+                </div>
+
             </div>
 
-        </div>
-
-      {/*<div className="addRightBtn addBtn btn">
+            {/*<div className="addRightBtn addBtn btn">
         <AddBtn />
       </div>
       <div className="addLeftBtn addBtn btn">
@@ -110,18 +108,17 @@ export default function Palette() {
       </div>*/}
 
 
-
-      {appData.colorPanes
-        .sort((a, b) => a.panePosition - b.panePosition)
-        .map((v, vi) => (
-          <ColorPane
-            key={vi}
-            pane={v}
-            isLeft = {vi == 0}
-            isRight = {vi == appData.colorPanes.length-1}
-            isActive={appData.activePaneIdx === v.paneId}
-          />
-        ))}
-    </div>
-  );
+            {appData.colorPanes
+                .sort((a, b) => a.panePosition - b.panePosition)
+                .map((v, vi) => (
+                    <ColorPane
+                        key={vi}
+                        pane={v}
+                        isLeft={vi == 0}
+                        isRight={vi == appData.colorPanes.length - 1}
+                        isActive={appData.activePaneIdx === v.paneId}
+                    />
+                ))}
+        </div>
+    );
 }
